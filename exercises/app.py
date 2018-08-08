0dc99736b85b771c46bdb9e9924d4d5d24ad9b82
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:28:00 +0300

    fd

--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template(
-    "index.html")
+	fav_players= ["Rocky Rooster", "Lauren Llama", "Alice Alpaca"] 
+
+	return render_template("index.html",fav_players=fav_players,likes_same_sport=False)
 
 if __name__ == '__main__':
    app.run(debug = True)
